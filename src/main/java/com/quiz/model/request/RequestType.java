11aaf096e70e5d43deba74b0735a761c2e756684
Giorgi <ggela16@freeuni.edu.ge>
2020-07-21 23:05:53 +0400

    Request implementation

---
+++ src/main/java/com/quiz/model/request/RequestType.java
@@ -0,0 +1,7 @@
+package com.quiz.model.request;
+
+public class RequestType {
+    public final static String NOTE = "note";
+    public final static String CHALLENGE = "challenge";
+    public final static String FRIEND= "friendRequest";
+}
---
+++ src/main/tests/com/quiz/model/request/RequestTest.java
@@ -0,0 +1,19 @@
+package com.quiz.model.request;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RequestTest {
+
+    @Test
+    public void testConstructor(){
+        Request request = new Request("from","to",RequestType.NOTE, "body", -1);
+        assertEquals("from", request.getFromUser());
+        assertEquals("to", request.getToUser());
+        assertEquals("body", request.getBody());
+        assertEquals(RequestType.NOTE, request.getType());
+        assertEquals(-1, request.getId());
+    }
+
+}
